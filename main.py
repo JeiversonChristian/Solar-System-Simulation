be97c9eece68baa9a0efd5834ed0ca7ff3a353ff
JeiversonChristian <jeiversonc@gmail.com>
2023-06-18 17:04:41 -0300

    acrescentando mercurio e venus e arrumando escala da terra

--- main.py
+++ main.py
@@ -17,10 +17,12 @@ ALTURA_TELA = 800
 JANELA = pygame.display.set_mode( (LARGURA_TELA, ALTURA_TELA) )
 pygame.display.set_caption("Solar System Simulation")
 
-PRETO = (0, 0, 0)
-AMARELO = (255, 255, 0)
-AZUL = (100, 149, 237)
-LARANJA = (255, 165, 0)
+COR_FUNDO_UNIVERSO = (0, 0, 0)
+COR_SOL = (255, 255, 0)
+COR_TERRA = (100, 149, 237)
+COR_MARTE = (255, 165, 0)
+COR_MERCURIO = (139, 126, 102)
+COR_VENUS = (255, 87, 34)
 
 # UA -> Unidades Astronômicas
 # 1,496 x 10^11 metros
@@ -59,31 +61,43 @@ def main():
 
     relogio = pygame.time.Clock()
 
-    raio_terra = (6.371 * 10**3) * escala * (10**6)
+    raio_terra = (6.371 * 10**3) * escala * (7*10**5)
     raio_sol = raio_terra * (110**(1/2))
+    raio_mercurio = raio_terra / 1.7**(1/2)
+    raio_venus = raio_terra / 0.95**(1/2)
     raio_marte = raio_terra / 2**(1/2)
 
-    sol = Orbe(0, 0, raio_sol, AMARELO, 1.98892 * 10**30 )
+    sol = Orbe(0, 0, raio_sol, COR_SOL, 1.98892 * 10**30 )
     sol.eh_sol = True
 
-    terra = Orbe(-1*UA, 0, raio_terra, AZUL, 5.9722 * 10**24)
-    marte = Orbe(-1.524*UA, 0, raio_marte, LARANJA, 6.39 * 10**23)
+    mercurio = Orbe(0.387*UA, 0, raio_mercurio, COR_MERCURIO, 3.30 * 10**23)
+    venus = Orbe(0.723*UA, 0, raio_venus, COR_VENUS, 4.8685 * 10**24)
+    terra = Orbe(-1*UA, 0, raio_terra, COR_TERRA, 5.9722 * 10**24)
+    marte = Orbe(-1.524*UA, 0, raio_marte, COR_MARTE, 6.39 * 10**23)
 
-    orbes = [sol, terra, marte]
+    orbes = [sol, mercurio, venus, terra, marte]
 
     rodando = True
     while rodando == True:
         # 60 FPS no máximo
         relogio.tick(60)
 
-        raio_terra = (6.371 * 10**3) * escala * (10**6)
+        raio_terra = (6.371 * 10**3) * escala * (7*10**5)
         terra.raio = raio_terra
+
         raio_sol = raio_terra * (110**(1/2))
         sol.raio = raio_sol
+
+        raio_mercurio = raio_mercurio = raio_terra / 1.7**(1/2)
+        mercurio.raio = raio_mercurio
+
+        raio_venus = raio_terra / 0.95**(1/2)
+        venus.raio = raio_venus
+
         raio_marte = raio_terra / 2**(1/2)
         marte.raio = raio_marte
 
-        JANELA.fill(PRETO)
+        JANELA.fill(COR_FUNDO_UNIVERSO)
 
         for orbe in orbes:
             orbe.desenhar(JANELA, escala)
@@ -91,25 +105,21 @@ def main():
         pygame.display.update()
 
         for evento in pygame.event.get():
+
+            if evento.type == pygame.MOUSEWHEEL:
+                if evento.y > 0:  # Rolagem para cima
+                    fator_escala += 3
+                    evento.y = 0
+
+                elif evento.y < 0:  # Rolagem para baixo
+                    fator_escala -= 3
+                    evento.y = 0
+
+            escala = fator_escala / UA
+            JANELA.fill(COR_FUNDO_UNIVERSO)
+
             if evento.type == pygame.QUIT:
                 rodando = False
-
-        if evento.type == pygame.MOUSEBUTTONDOWN or evento.type == pygame.MOUSEBUTTONUP:
-            if evento.button == 4:  # Rolagem para cima
-                print("Rolagem para cima")
-            elif evento.button == 5:  # Rolagem para baixo
-                print("Rolagem para baixo")
-
-        if evento.type == pygame.MOUSEWHEEL:
-            if evento.y > 0:  # Rolagem para cima
-                fator_escala += 3
-                evento.y = 0
-                escala = fator_escala / UA
-            elif evento.y < 0:  # Rolagem para baixo
-                fator_escala -= 3
-                evento.y = 0
-                escala = fator_escala / UA
-            JANELA.fill(PRETO)
 
     pygame.quit()
 
